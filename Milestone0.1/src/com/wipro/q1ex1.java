c9252ff40236decfa40b70745f0ea45959dd3508
DELL <DELL@DESKTOP-UCGGIPJ.mshome.net>
2020-06-24 11:51:32 +0530

    First commit

---
+++ Milestone0.1/src/com/wipro/q1ex1.java
@@ -0,0 +1,12 @@
+package com.wipro;
+
+public class q1ex1 {
+	
+	public static void main(String[] args) {
+		
+		String a=args[0];
+		String b=args[1];
+		System.out.println(a+" "+"Technologies"+" "+b);
+	}
+
+}
